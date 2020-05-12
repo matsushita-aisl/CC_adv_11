98c5abae352eb15b0afbe5601342eb61b419b238
matsushita <matsushita@aisl.cs.tut.ac.jp>
2020-05-12 11:56:38 +0900

    init

---
+++ src/Client/SocketClientTest.java
@@ -0,0 +1,8 @@
+package Client;
+
+public class SocketClientTest{
+
+	public static void main(String[] args){
+
+	}
+}
---
+++ src/Server/SocketServerTest.java
@@ -0,0 +1,8 @@
+package Server;
+
+public class SocketServerTest{
+
+	public static void main(String[] args){
+
+	}
+}
